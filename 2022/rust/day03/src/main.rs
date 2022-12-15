5ed9e79148935090f34c4f13ef5266e516ff21b3
Ryan Lagasse <16790838+monoflo@users.noreply.github.com>
2022-12-15 17:36:45 -0500

    AoC-2022-03.2 Progress

--- 2022/rust/day03/src/main.rs
+++ 2022/rust/day03/src/main.rs
@@ -1,20 +1,14 @@
 use std::collections::HashSet;
 use std::io::Read;
-use std::str::FromStr;
-
-fn split_at_mid(input: &str) -> (&str, &str) {
-    let mid = input.len() / 2;
-    input.split_at(mid)
-}
 
 type Item = char;
 type ItemSet = HashSet<Item>;
 
 /// Computes the priority for the given `Item`.
-fn priority(item: Item) -> anyhow::Result<usize> {
+fn priority(item: &Item) -> anyhow::Result<usize> {
     static ASCII_A_LOWER: usize = 'a' as usize;
     static ASCII_A_UPPER: usize = 'A' as usize;
-    let ascii = item as usize;
+    let ascii = *item as usize;
     match item {
         'a'..='z' => Ok(ascii - ASCII_A_LOWER + 1),
         'A'..='Z' => Ok(ascii - ASCII_A_UPPER + 27),
@@ -22,91 +16,93 @@ fn priority(item: Item) -> anyhow::Result<usize> {
     }
 }
 
-/// Represents a rucksack containing supplies.
-struct Rucksack {
-    /// An array of sets representing rucksack compartments; contains each unique item in the compartment.
-    compartment: [ItemSet; 2],
-}
-
-impl Rucksack {
-    /// Retrieves the common item between the two compartments, of which there is assumed to be
-    /// only one.
-    fn common_item(&self) -> anyhow::Result<Item> {
-        // Retrieve an iterator of the items common to both compartments.
-        let mut iter = self.compartment[0].intersection(&self.compartment[1]);
-
-        // Expect the first item in the iterator.
-        let item = iter.next().unwrap();
-
-        // Check that there are no other common items between the rucksacks.
-        assert_eq!(0, iter.count());
-
-        Ok(*item)
-    }
-}
-
-impl FromStr for Rucksack {
-    type Err = &'static str;
-
-    /// Attempt to parse `Rucksack` from a string.
-    fn from_str(contents: &str) -> Result<Self, Self::Err> {
-        let (mut c1, mut c2) = (ItemSet::new(), ItemSet::new());
-        let (lhs, rhs) = split_at_mid(contents);
-        lhs.chars().for_each(|chr| {
-            c1.insert(chr);
-        });
-        rhs.chars().for_each(|chr| {
-            c2.insert(chr);
-        });
-        Ok(Rucksack {
-            compartment: [c1, c2],
-        })
-    }
-}
-
 /// Read, return data from the input file.
-fn read_input() -> anyhow::Result<Vec<Rucksack>> {
+fn read_input() -> anyhow::Result<String> {
     let path = std::path::PathBuf::from("input");
     let mut file = std::fs::File::open(path)?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
-    let rucksacks: Vec<Rucksack> = data
-        .lines()
-        .map(|line| Rucksack::from_str(line).unwrap())
-        .collect();
-    Ok(rucksacks)
+    Ok(data)
 }
 
 /// Compute the sum of priorities of all the items common to both compartments of each rucksack.
-fn part_one(data: &[Rucksack]) -> anyhow::Result<usize> {
-    Ok(data
-        .iter()
-        .map(|sack| sack.common_item().unwrap())
-        .map(|item| priority(item).unwrap())
-        .reduce(|sum, prio| sum + prio)
-        .unwrap())
+fn part_one(data: &str) -> anyhow::Result<usize> {
+    let mut sum: usize = 0;
+
+    for line in data.lines() {
+        // create sets for both compartments
+        let (mut lset, mut rset) = (ItemSet::new(), ItemSet::new());
+
+        // split line in half at the midpoint
+        let (lstr, rstr) = line.split_at(line.len() / 2);
+
+        // insert items from each compartment into respective sets
+        lstr.chars().for_each(|c| {
+            lset.insert(c);
+        });
+        rstr.chars().for_each(|c| {
+            rset.insert(c);
+        });
+
+        // determine items common to both compartments
+        let mut itcommon = lset.intersection(&rset);
+
+        // expect the first item in the iterator and no more
+        let item = itcommon.next().unwrap();
+        assert_eq!(0, itcommon.count());
+
+        // determine priority of common item and add to sum
+        sum += priority(item)?;
+    }
+
+    Ok(sum)
+}
+
+fn part_two(data: &str) -> anyhow::Result<usize> {
+    let mut sum: usize = 0;
+    let mut elfidx: usize = 0;
+    let mut elfgroup: [ItemSet; 3] = Default::default();
+
+    let mut itlines = data.lines();
+    while let Some(sack) = itlines.next() {
+        // clear previous sack to overwrite
+        elfgroup[elfidx].clear();
+
+        // insert items from each sack into the set for the correct group member
+        sack.chars().for_each(|c| {
+            elfgroup[elfidx].insert(c);
+        });
+
+        // determine items common to each group member
+        let itgroup = elfgroup.iter();
+        // https://www.reddit.com/r/rust/comments/5v35l6/intersection_of_more_than_two_sets/
+        // let common = itgroup
+        // .next()
+        // .map(|set| itgroup.fold(set, |s1, s2| s1 & s2))
+        // .unwrap();
+        // let itcommon = common.iter();
+
+        // expect the first item in the iterator and no more
+        // let item = itcommon.next().unwrap();
+        // assert_eq!(0, itcommon.count());
+
+        // determine priority of common item and add to sum
+        // sum += priority(item)?;
+    }
+
+    Ok(sum)
 }
 
 fn main() -> anyhow::Result<()> {
     let data = read_input()?;
-    println!(
-        "Part One - Sum of priorities of common items: {}",
-        part_one(&data)?
-    );
+    println!("Part One: {}", part_one(&data)?);
+    println!("Part Two: {}", part_two(&data)?);
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn split_at_mid_valid() {
-        let input = String::from("Hello World!");
-        let (lhs, rhs) = split_at_mid(&input);
-        assert_eq!(lhs, "Hello ");
-        assert_eq!(rhs, "World!");
-    }
 
     #[test]
     fn priority_valid() -> anyhow::Result<()> {
